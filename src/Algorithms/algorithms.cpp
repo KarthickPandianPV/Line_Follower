3b0958d7976bce6b48a8d3ffbff419f86484093b
mukilan <cobrapocalypse@gmail.com>
2024-12-16 19:37:42 +0530

    Priority order updated

--- src/Algorithms/algorithms.cpp
+++ src/Algorithms/algorithms.cpp
@@ -3,17 +3,51 @@
 
 #include "algorithms.hpp"
 
-namespace line_follower {
-Algorithms::Algorithms() {}
-Algorithms::~Algorithms() {}
+namespace line_follower
+{
+  Algorithms::Algorithms() {}
+  Algorithms::~Algorithms() {}
 
-void Algorithms::free_run() {}
+  void Algorithms::free_run() {}
 
-void Algorithms::dfs(uint8_t mode) {
-  // Implement BFS algorithm here
-  // switch (mode) { case 0: }
-}
+  void Algorithms::dfs(uint8_t preference)
+  {
+    switch (preference)
+    {
+    case 0:
+      priorityOrder[0] = 'S';
+      priorityOrder[1] = 'R';
+      priorityOrder[2] = 'L';
+      break;
+    case 1:
+      priorityOrder[0] = 'S';
+      priorityOrder[1] = 'L';
+      priorityOrder[2] = 'R';
+      break;
+    case 2:
+      priorityOrder[0] = 'R';
+      priorityOrder[1] = 'S';
+      priorityOrder[2] = 'L';
+      break;
+    case 3:
+      priorityOrder[0] = 'R';
+      priorityOrder[1] = 'L';
+      priorityOrder[2] = 'S';
+    case 4:
+      priorityOrder[0] = 'L';
+      priorityOrder[1] = 'S';
+      priorityOrder[2] = 'R';
+    case 5:
+      priorityOrder[0] = 'L';
+      priorityOrder[1] = 'R';
+      priorityOrder[2] = 'S';
+    default:
+      priorityOrder[0] = 'S';
+      priorityOrder[1] = 'R';
+      priorityOrder[2] = 'L';
+    }
+  }
 
-}  // namespace line_follower
+} // namespace line_follower
 
-#endif  // LINE_FOLLOWER_SRC_ALGORITHMS_ALGORITHMS_CPP
+#endif // LINE_FOLLOWER_SRC_ALGORITHMS_ALGORITHMS_CPP
--- src/Algorithms/algorithms.hpp
+++ src/Algorithms/algorithms.hpp
@@ -3,20 +3,23 @@
 #include <Arduino.h>
 
 #include "Motor_Driver/motor_driver.hpp"
-namespace line_follower {
+namespace line_follower
+{
 
-class Algorithms {
- private:
-  MotorDriver motor_driver_;
+  class Algorithms
+  {
+  private:
+    MotorDriver motor_driver_;
+    char priorityOrder[3] = {};
 
- public:
-  Algorithms();
-  ~Algorithms();
+  public:
+    Algorithms();
+    ~Algorithms();
 
-  void free_run();
-  void dfs(uint8_t mode);
-  void bfs();
-};
-}  // namespace line_follower
+    void free_run();
+    void dfs(uint8_t mode);
+    void bfs();
+  };
+} // namespace line_follower
 
-#endif  // LINE_FOLLOWER_SRC_ALGORITHMS_ALGORITHMS_HPP
+#endif // LINE_FOLLOWER_SRC_ALGORITHMS_ALGORITHMS_HPP
